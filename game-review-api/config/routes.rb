fe8b02aa68f07284ec83785add68b573e66e7e2e
AntRod18 <antrod1895@gmail.com>
2021-05-25 14:09:01 -0400

    change backend file name

---
+++ game-review-api/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :games do 
+    resources :reviews
+  end
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ game-review-api/db/migrate/20210525174916_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.text :game_title
+      t.string :score
+      t.string :snippet
+      t.belongs_to :game, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
