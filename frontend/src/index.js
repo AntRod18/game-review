f7e3fe18c0add742c96d8839790a6dc0bd5aa0cb
AntRod18 <antrod1895@gmail.com>
2021-05-26 14:33:01 -0400

    add function to fetch games

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchGames();
+})
+
+
+
+function fetchGames() {
+    const gamesContainer = document.getElementById("games-container")
+    fetch("http://localhost:3000/games")
+    .then(r => r.json())
+    .then(data => {
+        data.forEach(game => gamesContainer.innerHTML += `<li>${game.title}</li>`)
+    })
+    .catch(err => console.warn(err))
+}
